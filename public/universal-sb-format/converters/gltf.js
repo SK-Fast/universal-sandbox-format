2c75b64e0bf299f625c2cb9941e212150ebd11d1
devy <60780448+SK-Fast@users.noreply.github.com>
2023-10-07 23:50:14 +0700

    GLTF Export

--- public/universal-sb-format/converters/gltf.js
+++ public/universal-sb-format/converters/gltf.js
@@ -1,30 +1,46 @@
 const uF = require('../utils/universal-format.js')
 
 const metadata = {
-  platformName: "GLTF Model [EXPERIMENTAL]",
+  platformName: "GLTF Model",
   supportedVersion: `2.0`,
   fileExtension: `.glb`,
   fileType: "json"
 }
 
+function rad2Deg(angle) {
+  return 2 * Math.PI * (angle / 360)
+}
+
 function fromUniversal(u) {
   return new Promise((resolve) => {
     const scene = new THREE.Scene();
+    const envRoot = new THREE.Object3D()
+
+    envRoot.name = "USBF_Export"
+    scene.add(envRoot)
 
     for (const part of u.parts) {
       const geometry = new THREE.BoxGeometry(part.size.x, part.size.y, part.size.z);
-      const material = new THREE.MeshBasicMaterial({
-        color: {
-          r: part.color.r,
-          g: part.color.g,
-          b: part.color.b,
-        }
+      const material = new THREE.MeshStandardMaterial({
+        color: part.color.hex
       });
-      const cube = new THREE.Mesh(geometry, material);
-      cube.position = new THREE.Vector3(part.pos.x, part.pos.y, part.pos.z)
+      const cube = new THREE.Mesh(geometry);
+      cube.material = material
       cube.name = part.name
-      scene.add(cube);
+      cube.position.x = part.pos.x
+      cube.position.y = part.pos.y
+      cube.position.z = part.pos.z
+
+      cube.rotation.x = rad2Deg(part.rot.x)
+      cube.rotation.y = rad2Deg(part.rot.y)
+      cube.rotation.z = rad2Deg(part.rot.z)
+
+      envRoot.add(cube);
     }
+
+    envRoot.scale.setX(-1)
+    envRoot.scale.setY(1)
+    envRoot.scale.setZ(1)
 
     const exporter = new THREE.GLTFExporter();
 
